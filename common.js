53d8f20da9b3e04e58bb898934f84f1123abab05
Anna Potyomkina <64991134+D-our@users.noreply.github.com>
2020-08-04 23:26:26 +0300

    Add files via upload

---
+++ common.js
@@ -0,0 +1,46 @@
+
+    const students = [
+        {
+            name: 'John Smith',
+            marks: [10, 8, 6, 9, 8, 7 ]
+        },
+        {
+            name: 'John Doe',
+            marks: [ 9, 8, 7, 6, 7 ]
+        },
+        {
+            name: 'Thomas Anderson',
+            marks: [6, 7, 10, 8 ]
+        },
+        {
+            name: 'Jean-Baptiste Emanuel Zorg',
+            marks: [10, 9, 8, 9 ]
+        }
+    ]
+
+    const arrOfAllGrades =   arrOfAllMarks();
+
+    function averageStudentMark (student) {
+        return student.marks.reduce((mark, val) => mark + val, 0);
+    }
+
+    console.log(averageStudentMark(students[0]));
+
+    function arrOfAllMarks () {
+        return students.flatMap((item) => item.marks);
+
+    }
+
+
+    function averageGroupMark (arrOfAllGrades) {
+
+        let result =  arrOfAllGrades.reduce((a, b) => a + b);
+
+        return result / arrOfAllGrades.length;
+    }
+
+   console.log(averageGroupMark(arrOfAllGrades));
+
+
+
+
